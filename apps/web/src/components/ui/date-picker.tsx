9e3ab8da60db8c0024767ac1aed7fad450cb3398
Thomas <thomas.demathieu@gmail.com>
2026-08-08 17:37:06 +0200

    FIX: try modal date picker iphone fix

--- apps/web/src/components/ui/date-picker.tsx
+++ apps/web/src/components/ui/date-picker.tsx
@@ -48,7 +48,7 @@ export function DatePicker({
   };
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} modal>
       <PopoverTrigger asChild>
         <Button
           id={id}
